25f34ec464e6aeb8a49807e33dfc8fa233557546
krish090907 <24331a05d7@mvgrce.edu.in>
2026-03-29 16:32:18 +0530

    Add files via upload

---
+++ week1_1.py
@@ -0,0 +1,11 @@
+n1=15
+str1="krish"
+n2=112.63
+char='p'
+print("num1: ",n1,"type of n1: ",type(n1))
+print("str1: ",str1,"type of str1: ",type(str1))
+print("num2: ",n2,"type of n2: ",type(n2))
+print("char: ",char,"type of char: ",type(char))
+print("result: ",n2/n1,"type of result: ",type(n2/n1))
+print("value: ",ord(char),"type of value: ",type(ord(char)))
+print("floor rounding: ",n2//n1)
