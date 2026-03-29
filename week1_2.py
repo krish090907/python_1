29b49d376f3753d9e0273238676cba82164713e0
krish090907 <24331a05d7@mvgrce.edu.in>
2026-03-29 16:46:53 +0530

    Add files via upload

---
+++ week1_2.py
@@ -0,0 +1,5 @@
+print(f"the result of 5+3*2 is: {5+3*2}")
+print(f"the result of 2*3**2 is: {2*3**2}")
+print(f"the result of 2**3**2 is: {2**3**2}")
+print(f"the result of (2**3)**2 is: {(2**3)**2}")
+
